2df6e2b8a8055ab9557c67f02f8c10907ab0907b
Deep Majumder <deep.majumder2019@gmail.com>
2022-03-07 17:29:57 +0530

    Added signup page

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -6,3 +6,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+
+@bp.route('/signup', methods=['GET'])
+def signup():
+    return render_template('signup.html')
