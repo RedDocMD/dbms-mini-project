b5f03750fb9d5142769f1e150cdb33604ea65fa9
Deep Majumder <deep.majumder2019@gmail.com>
2022-03-08 18:47:34 +0530

    Basic check on structure

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flaskr.db import get_db
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -8,6 +10,32 @@ def login():
     return render_template('login.html')
 
 
-@bp.route('/signup', methods=['GET'])
+@bp.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        address = request.form['address']
+        email = request.form['email']
+        passwd = request.form['passwd']
+        confirm_passwd = request.form['confirm-passwd']
+
+        db = get_db()
+
+        errors = []
+        if not name:
+            errors.append("Name is required")
+        if not address:
+            errors.append("Address is required")
+        if not email:
+            errors.append("Email is required")
+        if not passwd:
+            errors.append("Password is required")
+        if passwd != confirm_passwd:
+            errors.append("Passwords don't match")
+
+        if not errors:
+            return redirect(url_for('index'))
+
+        for error in errors:
+            flash(error)
     return render_template('signup.html')
