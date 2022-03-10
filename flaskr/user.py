0de1121defe8103bf0e363f832aa22bb7247b61c
Deep Majumder <deep.majumder2019@gmail.com>
2022-03-10 13:29:01 +0530

    Logic for cart put in place

--- flaskr/user.py
+++ flaskr/user.py
@@ -10,8 +10,30 @@ bp = Blueprint('user', __name__, url_prefix='/user')
 @bp.route('/cart', methods=['GET'])
 @login_required
 def cart():
+    db = get_db()
+    user_id = g.user['userId']
+
+    productsData = db.execute(
+        ('SELECT DISTINCT p.productId, p.productName, p.productDescription, c.quantity, sp.price, sp.discount  '
+         'FROM Product p, Cart c, SellerProduct sp '
+         'WHERE p.productId = c.productId AND c.userId = ? AND sp.productId = p.productId'), (user_id,)).fetchall()
+
     products = []
     prices = {}
-    discountedPrices = {}
+    discounted_prices = {}
+
+    for row in productsData:
+        product = {
+            'productId': row['productId'],
+            'productName': row['productName'],
+            'productDescription': row['productDescription']
+        }
+        products.append(product)
+        price = row['price']
+        discount = row['discount']
+        prices[product['productId']] = price
+        discounted_prices[product['productId']] = int(price *
+                                                      (100.0 - discount) / 100.0)
+
     return render_template('cart.html',
-                           products=products, prices=prices, discountedPrices=discountedPrices)
+                           products=products, prices=prices, discounted_prices=discounted_prices)
