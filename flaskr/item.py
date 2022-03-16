7186453a5aa64ac23af447618e927f09c6091dd2
Deep Majumder <deep.majumder2019@gmail.com>
2022-03-16 14:37:10 +0530

    Item creation from existing item

--- flaskr/item.py
+++ flaskr/item.py
@@ -65,3 +65,38 @@ def create():
         for error in errors:
             flash(error)
     return render_template('add_new_item.html')
+
+
+@bp.route('/assign', methods=['GET', 'POST'])
+@login_required
+def assign():
+    db = get_db()
+    if request.method == 'POST':
+        selected_id = request.form['selected_product']
+        print(f'Selected_id = {selected_id}')
+        return redirect(url_for('item.assign_values', product_id=selected_id))
+    products = db.execute('SELECT * FROM Product').fetchall()
+    return render_template('assign_item.html', products=products)
+
+
+@bp.route('/assign/<product_id>', methods=['GET', 'POST'])
+@login_required
+def assign_values(product_id):
+    db = get_db()
+    if request.method == 'POST':
+        price = request.form['price']
+        disc = request.form['discount']
+        qty = request.form['quantity']
+        errors = []
+        try:
+            db.execute(
+                'INSERT INTO SellerProduct (productId, sellerId, price, discount, quantity) VALUES (?, ?, ?, ?, ?)',
+                (product_id, g.user['userId'], price, disc, qty))
+            db.commit()
+        except db.IntegrityError as e:
+            errors.append(f'Failed to add {e}')
+        else:
+            return redirect(url_for('user.profile'))
+    product = db.execute(
+        'SELECT * FROM Product WHERE productId = ?', (product_id, )).fetchone()
+    return render_template('assign_item_data.html', product=product)
